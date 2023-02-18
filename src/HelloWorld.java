7098cc7180cff6aa9c1f9f0c6e2cf99565814745
Michael Smith <m.j.smith10@newcastle.ac.uk>
2023-02-18 15:12:14 +0000

    change to hello world

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,9 @@ public class HelloWorld {
     public static void main(String[] args)
     {
         System.out.println("Hello World");
+        String name = "Michael";
+        String output = "My name is" + name + "!" + "i am";
+        int age = (45);
+        System.out.println (output + age);
     }
 }
